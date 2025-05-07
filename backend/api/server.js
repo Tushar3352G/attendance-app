4d9a1859d67b9a4836ded814365958e3d60b3357
Tushar3352G <vermatushar267@gmail.com>
2025-05-08 00:23:03 +0530

    fix serverless issue

--- backend/api/server.js
+++ backend/api/server.js
@@ -11,7 +11,7 @@ app.use(cors({
   origin:'*',
 }));
 
-app.options("*", cors()); 
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,6 @@ app.use(cors({
   origin:'*',
 }));
 
-app.options("*", cors()); 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
